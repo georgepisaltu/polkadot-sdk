38be3350020932f506217b709333aa0769772d5b
georgepisaltu <george.pisaltu@parity.io>
2023-12-15 13:09:58 +0200

    Fix unused imports in transaction payment weight
    Signed-off-by: georgepisaltu <george.pisaltu@parity.io>

--- substrate/frame/transaction-payment/src/weights.rs
+++ substrate/frame/transaction-payment/src/weights.rs
@@ -20,7 +20,7 @@
 #![allow(unused_imports)]
 #![allow(missing_docs)]
 
-use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
+use frame_support::weights::Weight;
 use core::marker::PhantomData;
 
 /// Weight functions needed for pallet_utility.
